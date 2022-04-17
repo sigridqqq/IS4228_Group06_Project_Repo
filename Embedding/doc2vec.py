90252fd0e9c9ece792bdcddd8fc3667fffe676a8
ranyd18 <62640778+ranyd18@users.noreply.github.com>
2022-04-17 23:28:05 +0800

    Update doc2vec.py

--- Embedding/doc2vec.py
+++ Embedding/doc2vec.py
@@ -53,16 +53,16 @@ for s in tqdm(test['cleaned_text']):
 
 model = 'doc2vec'
 vector_size = 60
-doc2vec_filename = f'saved_model/{category}_doc2vec_model.sav'
+# doc2vec_filename = f'saved_model/{category}_doc2vec_model.sav'
 
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 tagged_text = [TaggedDocument(d, [i]) for i, d in enumerate(tokenized_sent)]
 print("currently learning doc2vec model")
 doc2vec = Doc2Vec(tagged_text, vector_size = vector_size, window = 2, min_count = 1, epochs = 100)
-pickle.dump(doc2vec, open(doc2vec_filename, 'wb'))
+# pickle.dump(doc2vec, open(doc2vec_filename, 'wb'))
 
-print("currently loading doc2vec model")
-doc2vec = pickle.load(open(doc2vec_filename, 'rb'))
+# print("currently loading doc2vec model")
+# doc2vec = pickle.load(open(doc2vec_filename, 'rb'))
 
 print("currently encoding train text")
 train_vector_array = np.array([])
@@ -80,7 +80,7 @@ resultant_train_vector_array = np.append(resultant_train_vector_array, train_vec
 train_vector_array = resultant_train_vector_array.reshape([-1, vector_size])
 
 print(train_vector_array.shape)
-with open(f'processed_text/{category}_{model}_train.npy', 'wb') as f:
+with open(f'text_processing/{category}_{model}_train.npy', 'wb') as f:
     np.save(f, train_vector_array)
 
 
@@ -89,7 +89,7 @@ val_vector_array = np.array([])
 for i in tqdm(tokenized_sent_val):
     val_vector_array = np.append(val_vector_array, doc2vec.infer_vector(i))
 val_vector_array = val_vector_array.reshape([-1, vector_size])
-with open(f'processed_text/{category}_{model}_val.npy', 'wb') as f:
+with open(f'text_processing/{category}_{model}_val.npy', 'wb') as f:
     np.save(f, val_vector_array)
 
 print("currently encoding test text")
@@ -97,11 +97,11 @@ test_vector_array = np.array([])
 for i in tqdm(tokenized_sent_test):
     test_vector_array = np.append(test_vector_array, doc2vec.infer_vector(i))
 test_vector_array = test_vector_array.reshape([-1, vector_size])
-with open(f'processed_text/{category}_{model}_test.npy', 'wb') as f:
+with open(f'text_processing/{category}_{model}_test.npy', 'wb') as f:
     np.save(f, test_vector_array)
 
 # to load the saved embedding 
-# with open(f'processed_text/{model}_train.npy', 'rb') as f:
+# with open(f'text_processing/{model}_train.npy', 'rb') as f:
     # train_vector_array_reload = np.load(f)
 
 print('doc2vec embedding shape for train, val, test is respectively: ', train_vector_array.shape, val_vector_array.shape, test_vector_array.shape)
