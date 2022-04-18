ee8d1817dac360ba7c9ded1811f713a729691b01
sigridqqq <sigrid.qiu.shichao@gmail.com>
2022-04-18 16:25:59 +0800

    Update detect_language.py

--- Data Preprocessing/detect_language.py
+++ Data Preprocessing/detect_language.py
@@ -6,26 +6,16 @@ def save_obj(obj, name):
     with open(name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-# To download train.csv, go to https://drive.google.com/file/d/1dkY35gnkHGUDRi4flC8rV3vk_P1LceOL/view?usp=sharing
-# To download val.csv,   go to https://drive.google.com/file/d/1SCzPAb_CAw7AOeLSmPYRsSl-hEmzpKU6/view?usp=sharing
-# To download test.csv,  go to https://drive.google.com/file/d/1xMcG0yjZKEiU85pt92IHIDDWWPZriWcR/view?usp=sharing 
-
+# To download yelp_review.csv, go to https://drive.google.com/file/d/17uPXOVdgIPFMGH8uScdK6qclE0sby7Ks/view?usp=sharing
 # Change filepath here accordingly
-train = pd.read_csv("/home/q/qshichao/BT4222/Archive/yelp2017_train.csv")
-val   = pd.read_csv("/home/q/qshichao/BT4222/Archive/yelp2017_val.csv")
-test  = pd.read_csv("/home/q/qshichao/BT4222/Archive/yelp2017_test.csv")
 
-train = train.drop('Unnamed: 0',1)
-val   = val.drop('Unnamed: 0',1)
-test  = test.drop('Unnamed: 0',1)
-
-df = pd.concat([train, val,test], axis=0)
+df = pd.read_csv("/home/q/qshichao/BT4222/Archive/yelp_review.csv")
 df.dropna(inplace=True)
 df['language'] = ''
 
 print('start detect')
 
-print('total number of jocs = ' + str(len(df)))
+print('total number of jobs = ' + str(len(df)))
 print()
 
 for i in range(len(df)):
