a802ac3db0365d91b2bd04f547b0c5c6c9e4070d
sigridqqq <sigrid.qiu.shichao@gmail.com>
2022-04-18 12:57:30 +0800

    Added Data Download Link

--- Data Preprocessing/detect_language.py
+++ Data Preprocessing/detect_language.py
@@ -6,6 +6,11 @@ def save_obj(obj, name):
     with open(name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
+# To download train.csv, go to https://drive.google.com/file/d/1dkY35gnkHGUDRi4flC8rV3vk_P1LceOL/view?usp=sharing
+# To download val.csv,   go to https://drive.google.com/file/d/1SCzPAb_CAw7AOeLSmPYRsSl-hEmzpKU6/view?usp=sharing
+# To download test.csv,  go to https://drive.google.com/file/d/1xMcG0yjZKEiU85pt92IHIDDWWPZriWcR/view?usp=sharing 
+
+# Change filepath here accordingly
 train = pd.read_csv("/home/q/qshichao/BT4222/Archive/yelp2017_train.csv")
 val   = pd.read_csv("/home/q/qshichao/BT4222/Archive/yelp2017_val.csv")
 test  = pd.read_csv("/home/q/qshichao/BT4222/Archive/yelp2017_test.csv")
@@ -33,4 +38,5 @@ for i in range(len(df)):
         print("This row throws and error: ", df.iloc[i,-8])
 
 
-save_obj(df, 'original_review_language_detect')
+save_obj(df, 'original_review_language_detect') 
+# To download 'original_review_language_detect', go to https://drive.google.com/file/d/1e9pqur4iZUcRmi9hMK49U21m61kLCQ6S/view?usp=sharing
